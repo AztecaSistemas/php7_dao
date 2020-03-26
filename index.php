b7e4ff0bf47865b41d58e86edbdc0e3954018a35
Cesar Augusto Teixeira <teixeira.cesaraugusto@hotmail.com>
2020-03-26 20:16:30 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -6,8 +6,22 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+// Carrega um usuario pelo ID
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
+
+// Carrega a lista de usuarios (utiliza método static function)
+//$lists = Usuario::getList();
+//echo json_encode($lists);
+
+// Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+// Carrega um usuário informando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "!@$#.");
+echo $usuario;
 
 ?>
